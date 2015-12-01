04aec804fdfb8565a5dac20073256d9c31a10ba4
0207cyfarvaque <0207cyfarvaque@PC8817.domepfc.net>
2015-12-01 19:36:17 +0100

    Hello git commit

--- lesson11/src/epfc/cours3449/lesson11.java
+++ lesson11/src/epfc/cours3449/lesson11.java
@@ -16,6 +16,7 @@ public class lesson11 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello Git");
         // TODO code application logic here
     }
     
